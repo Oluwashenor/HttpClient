79ab4f3ef4d1b9e3cc262eae94234f04568ff81f
Adeshina <adeshiname@gmail.com>
2022-09-15 10:57:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyHttpClient
+{
+
+    public class Program
+    {
+
+        public void http()
+        {
+            //Create Client 
+            HttpClient client = new HttpClient();
+        
+        
+            //Close Connection and release resources
+            client.Dispose();   
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adesina");
+        }
+    }
+   
+}
+                
