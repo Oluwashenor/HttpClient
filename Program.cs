b729fc418508eb5054391c2add7c8145431e2710
Adeshina <adeshiname@gmail.com>
2022-09-16 02:14:52 +0100

    getasync

--- Program.cs
+++ Program.cs
@@ -5,18 +5,31 @@ namespace MyHttpClient
 
     public class Program
     {
+         string getUrl = "https://jsonplaceholder.typicode.com/posts/1";
 
         public void http()
         {
             //Create Client 
             HttpClient client = new HttpClient();
-        
+            
         
             //Close Connection and release resources
             client.Dispose();   
         }
+
+        static void httpGet()
+        {
+           string getUrl = "https://jsonplaceholder.typicode.com/posts/1";
+           HttpClient client = new HttpClient();
+           var uri = new Uri(getUrl);
+           Task<HttpResponseMessage> httpResponse = client.GetAsync(uri);
+           HttpResponseMessage httpResponseMessage = httpResponse.Result;
+           Console.WriteLine(httpResponseMessage.ToString());
+           client.Dispose();
+        }
         static void Main(string[] args)
         {
+            httpGet();
             Console.WriteLine("Adesina");
         }
     }
