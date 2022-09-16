53e31d0232f5ff8e86ad77d9bc1c119f39f91535
Adeshina <adeshiname@gmail.com>
2022-09-16 08:11:18 +0100

    httpclient

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace MyHttpClient
 {
@@ -22,14 +24,59 @@ namespace MyHttpClient
            string getUrl = "https://jsonplaceholder.typicode.com/posts/1";
            HttpClient client = new HttpClient();
            var uri = new Uri(getUrl);
-           Task<HttpResponseMessage> httpResponse = client.GetAsync(uri);
-           HttpResponseMessage httpResponseMessage = httpResponse.Result;
-           Console.WriteLine(httpResponseMessage.ToString());
-           client.Dispose();
+           
+            Task<HttpResponseMessage> httpResponse = client.GetAsync(uri);
+            HttpResponseMessage httpResponseMessage = httpResponse.Result;
+            Console.WriteLine(httpResponseMessage.ToString());
+
+            //Status Code
+            HttpStatusCode statusCode = httpResponseMessage.StatusCode;
+            Console.WriteLine("status Code =>"+statusCode);
+            Console.WriteLine("status Code =>"+(int)statusCode);
+           
+            //Response 
+            HttpContent responseContent = httpResponseMessage.Content;
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+            string data = responseData.Result;
+            Console.WriteLine(data);
+
+            client.Dispose();
+
         }
+
+
+        static void httpGetInXML()
+        {
+            string getUrl = "https://jsonplaceholder.typicode.com/posts/1";
+            //Headers
+            HttpClient client = new HttpClient();
+            HttpRequestHeaders requestHeader = client.DefaultRequestHeaders;
+            requestHeader.Add("Accept", "application/xml");
+            
+            var uri = new Uri(getUrl);
+
+            Task<HttpResponseMessage> httpResponse = client.GetAsync(uri);
+            HttpResponseMessage httpResponseMessage = httpResponse.Result;
+            Console.WriteLine(httpResponseMessage.ToString());
+
+            //Status Code
+            HttpStatusCode statusCode = httpResponseMessage.StatusCode;
+            Console.WriteLine("status Code =>" + statusCode);
+            Console.WriteLine("status Code =>" + (int)statusCode);
+
+            //Response 
+            HttpContent responseContent = httpResponseMessage.Content;
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+            string data = responseData.Result;
+            Console.WriteLine(data);
+
+            client.Dispose();
+
+        }
+
         static void Main(string[] args)
         {
-            httpGet();
+            httpGetInXML();
             Console.WriteLine("Adesina");
         }
     }
